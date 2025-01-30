16f0ce29d1c02d7e7627e4028adbe03419d74542
Omori0219 <oomoris0219@gmail.com>
2025-01-30 15:32:46 +0900

    fix: 🐛 ロガーをローカル環境でも動作するように修正

--- functions/src/utils/logger.js
+++ functions/src/utils/logger.js
@@ -1,4 +1,19 @@
-const { logger } = require("firebase-functions/logger");
+// ロガーの初期化
+let logger;
+
+try {
+  // Firebase Functionsの環境の場合
+  const functions = require("firebase-functions");
+  logger = functions.logger;
+} catch (error) {
+  // ローカル環境の場合
+  logger = {
+    info: (...args) => console.log("[INFO]", ...args),
+    error: (...args) => console.error("[ERROR]", ...args),
+    warn: (...args) => console.warn("[WARN]", ...args),
+    debug: (...args) => console.debug("[DEBUG]", ...args),
+  };
+}
 
 // ログ出力の統一
 function info(message) {
