a73cb3939ec08edde767cedb1423ee40087f06ac
Omori0219 <oomoris0219@gmail.com>
2025-01-30 17:10:53 +0900

    テスト用のコードを追加

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,9 @@
  */
 
 const { onSchedule } = require("firebase-functions/v2/scheduler");
+const { onRequest } = require("firebase-functions/v2/https");
 const { processAllPrefectures } = require("./src/core/batch-processor");
+const { processWeatherData } = require("./src/core/weather-mother");
 const logger = require("./src/utils/logger");
 
 // Create and deploy your first functions
@@ -36,3 +38,31 @@ exports.generateWeatherMessages = onSchedule(
     }
   }
 );
+
+// テスト用の関数を追加
+exports.generateWeatherMessagesTest = onRequest(async (req, res) => {
+  logger.info("テスト用天気予報メッセージ生成処理を開始します...");
+  logger.info("==== テストバッチ処理開始 ====");
+
+  // テスト用の都道府県コードリスト
+  const testAreaCodes = [
+    { code: "016000", name: "北海道" }, // 北日本
+    { code: "130000", name: "東京都" }, // 関東
+    { code: "270000", name: "大阪府" }, // 関西
+    { code: "471000", name: "沖縄県" }, // 南日本
+  ];
+
+  try {
+    // テスト用の都道府県のみ処理
+    for (const area of testAreaCodes) {
+      logger.info(`${area.name}の処理を開始...`);
+      await processWeatherData(area.code);
+      logger.info(`${area.name}の処理が完了しました！`);
+    }
+    logger.info("==== テストバッチ処理完了 ====");
+    res.send("Test completed successfully");
+  } catch (error) {
+    logger.error("テスト実行中にエラーが発生しました", error);
+    res.status(500).send(error);
+  }
+});
