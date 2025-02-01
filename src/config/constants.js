68e0913f79296e4069a5d98a45f523c51793eaa7
Omori0219 <oomoris0219@gmail.com>
2025-02-01 14:21:32 +0900

    refactor: 🔥 #0 重複したコードファイルの削除

--- src/config/constants.js
+++
@@ -1,5 +0,0 @@
-// 定数の集約
-module.exports = {
-  AREA_CODE: "130000",
-  COLLECTION_NAME: "weather_data",
-};
--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-// 日付操作ユーティリティ
-function generateDocumentId(areaCode) {
-  const today = new Date();
-  const dateStr = today.toISOString().slice(0, 10).replace(/-/g, "");
-  return `${dateStr}-${areaCode}`;
-}
-
-module.exports = { generateDocumentId };
