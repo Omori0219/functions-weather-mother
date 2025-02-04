26bcd02ba57f524d12c0047c6f91bf55e7c981bd
Omori0219 <oomoris0219@gmail.com>
2025-02-04 18:27:30 +0900

    feat: 🔧 気象庁APIレスポンスから週間予報データを除外する処理を追加

--- functions/src/clients/weather.js
+++ functions/src/clients/weather.js
@@ -6,6 +6,19 @@
 const { info, error } = require("../utils/logger");
 
 /**
+ * 気象庁APIのレスポンスから今日・明日の予報データのみを抽出
+ * @param {Array} weatherData - 気象庁APIのレスポンスデータ
+ * @returns {Object} 今日・明日の予報データ
+ */
+function filterWeatherData(weatherData) {
+  // 気象庁APIのレスポンスは配列形式で、
+  // weatherData[0] が今日・明日の予報
+  // weatherData[1] が週間予報
+  const todayAndTomorrowForecast = weatherData[0];
+  return todayAndTomorrowForecast;
+}
+
+/**
  * 気象庁APIから天気予報を取得
  * @param {string} areaId - 地域ID
  * @returns {Promise<Object>} 天気予報データ
@@ -23,9 +36,11 @@ async function getWeatherForecast(areaId) {
     }
 
     const weather = await response.json();
+    const filteredWeather = filterWeatherData(weather);
+
     info("天気予報データを取得完了", { areaId });
 
-    return weather;
+    return filteredWeather;
   } catch (err) {
     error("天気予報データの取得に失敗", { areaId, error: err });
     throw err;
