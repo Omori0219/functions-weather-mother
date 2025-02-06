1727a853cf3ca46c27ac5105427a5a688e4a5b86
Omori0219 <oomoris0219@gmail.com>
2025-02-06 22:41:38 +0900

    refactor: 🔥 #1 Remove saveWeatherData function from firebase.js

--- functions/src/clients/firebase.js
+++ functions/src/clients/firebase.js
@@ -32,43 +32,4 @@ async function saveDocument(collection, documentId, data) {
   }
 }
 
-async function saveWeatherData({
-  documentId,
-  areaCode,
-  weatherForecasts,
-  generatedMessage,
-  createdAt,
-}) {
-  try {
-    // デバッグ: createdAtの型と値を確認
-    logger.debug("Saving weather data", {
-      documentId,
-      areaCode,
-      createdAtType: typeof createdAt,
-      createdAtValue: createdAt,
-    });
-
-    const docRef = db.collection(COLLECTION_NAME).doc(documentId);
-
-    await docRef.set({
-      areaCode,
-      weatherForecasts,
-      generatedMessage,
-      createdAt: Timestamp.fromDate(createdAt),
-    });
-
-    logger.info("天気予報データの保存に成功しました", {
-      documentId,
-      areaCode,
-    });
-    return true;
-  } catch (error) {
-    logger.error("Firestoreへの書き込みに失敗しました", error, {
-      documentId,
-      areaCode,
-    });
-    throw error;
-  }
-}
-
-module.exports = { saveDocument, saveWeatherData };
+module.exports = { saveDocument };
